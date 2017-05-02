d17c9f2911e63ef59396562c4bb332e5acec74b5
Justin Teo <justin.tsw@gmail.com>
2017-05-02 14:32:40 +0800

    Created TestSort class with sort method and 6 assertions provided

---
+++ tc_sort.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+
+class TestSort < Test::Unit::TestCase
+  def sort(arr) #Sorting Algo to be tested
+    
+  end
+
+  def test_empty # Sort an Empty Array, result should be empty
+    assert_equal([], sort([]))
+  end
+
+  def test_one # Sort Array with 1 item, return the original array
+    assert_equal([1], sort([1]))
+  end
+
+  def test_two # Sort Array with 2 items (reversed order), return Array elements in asc order
+    assert_equal([1,2], sort([2,1]))
+  end
+
+  def test_three #Sort Array with 3 items, return Array elements in asc order
+    assert_equal([1,2,3], sort([1,2,3]))
+    assert_equal([1,2,3], sort([2,1,3]))
+    assert_equal([1,2,3], sort([1,3,2]))
+  end
+
+end
